4aa6fa8d1bd553ef46ab6311e5c8fefcf8426631
练建发 <958823465@qq.com>
2023-04-26 18:22:52 +0800

    fiber

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-###为什么在React17之前，每个React组件文件中都要引入 React ？
+### 为什么在React17之前，每个React组件文件中都要引入 React ？
 因为在React组件中写 DOM 结构最终都会被转化成`React.createElement`函数，比如
 ```js
 const elem = <div title="hi">hello mini react</div>
@@ -20,3 +20,37 @@ const elem = {
 }
 ```
 
+### 渲染方案
+在React 16之后，渲染真实DOM是用递归 + diff 的形式进行渲染的，这种方式有较大的缺陷就是当 虚拟DOM树 比较大的时候会耗费非常多的资源，而且一旦开始渲染，就不能停止了，直到渲染出完整的树结构。也就是说会造成主线程被持续占⽤，造成的后果就是主线程上的布局、动画等周期性任务就⽆法立即得到处理，造成视觉上的卡顿，影响⽤户体验。
+所以在 React 16 之后，将递归的无法中断的更新重构为异步的可中断更新，由于曾经用于递归的虚拟DOM数据结构已经无法满足需要。于是，全新的Fiber架构应运而生。
+
+### fiber 是什么
+1. 作为架构来说，之前React15的Reconciler采用递归的方式执行，数据保存在递归调用栈中，所以被称为stack Reconciler。React16的Reconciler基于Fiber节点实现，被称为Fiber Reconciler。
+2. 作为静态的数据结构来说，每个Fiber节点对应一个组件，保存了该组件的类型（函数组件/类组件/原生组件...）、对应的DOM节点等信息。
+3. 作为动态的工作单元来说，每个Fiber节点保存了本次更新中该组件改变的状态、要执行的工作（需要被删除/被插入页面中/被更新...）。
+
+fiber 可以看做是一个拥有虚拟DOM全部数据，以及全部前后状态的描述虚拟DOM树的链表
+
+fiber 的结构
+```js
+const FiberNode = {
+  //基础信息
+  tag,
+  type,
+  props,
+  stateNode,
+  
+  // 用于连接其他Fiber节点形成Fiber树
+  return,//指向父fiber
+  child,//指向子fiber
+  sibling,//指向兄弟fiber
+
+  // 副作用
+  effectTag,
+  nextEffect,
+  firstEffect,
+  lastEffect,
+
+  alternate
+}
+```
--- react/react-dom.js
+++ react/react-dom.js
@@ -1,26 +1,110 @@
+// 下一功能单元，是一个指针指向当前需要运行的功能单元
+let nextUnitOfWork = null;
+
 /**
  * 渲染虚拟DOM到目标上
  * @param {VNode} element 虚拟DOM
  * @param {HTMLElement} container  目标节点
  */
 export function render(element,container){
+  nextUnitOfWork = {
+    dom:container,
+    props:{
+      children:[element],
+    }
+  }
+}
+
+/**开始工作
+ * @param deadline 耗费时间和剩余时间
+ */
+function workLoop(deadline){
+  let shouldYield = false;
+  // 有需要执行的单元而且还有时间剩余不需要让出控制权
+  while(nextUnitOfWork && !shouldYield){
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    // 可以用时间小于 1 就让出控制权
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  // 空闲时间执行任务
+  window.requestIdleCallback(workLoop);
+}
+window.requestIdleCallback(workLoop);
+
+/**执行单元任务
+ * 1.将元素添加到 DOM
+ * 2.为元素的子元素创建fiber
+ * 3.选择下一个工作单元
+ */
+function performUnitOfWork(fiber){
+  //如果fiber的dom还没有创建就先创建
+  if(!fiber.dom){
+    fiber.dom = createDom(fiber);
+  }
+  // 如果fiber有父节点，将fiber.dom添加到父节点
+  if(fiber.parent){
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // 获取到当前fiber的孩子节点
+  const elements = fiber.props.children
+  // 索引
+  let index = 0
+  // 上一个兄弟节点
+  let prevSibling = null
+
+  // 遍历孩子节点
+  while (index < elements.length) {
+    const element = elements[index]
+    // 创建fiber
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    // 将第一个孩子节点设置为 fiber 的子节点
+    if (index === 0) {
+      fiber.child = newFiber
+    } else if(element) {
+      // 第一个之外的子节点设置为第一个子节点的兄弟节点
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+
+  // 寻找下一个孩子节点，如果有返回
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    // 如果有兄弟节点，返回兄弟节点
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    // 否则返回父节点
+    nextFiber = nextFiber.parent
+  }
+}
+
+//根据Fiber创建真实DOM
+function createDom(fiber){
   // 处理文本节点和标签节点
-  const dom = element.type === "TEXT_ELEMENT" 
+  const dom = fiber.type === "TEXT_ELEMENT" 
   ? document.createTextNode("")
-  : document.createElement(element.type);
+  : document.createElement(fiber.type);
   // 为节点绑定属性
   // 判断是否是属性
   const isProperty = key => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(key => {
-      dom[key] = element.props[key]
+      dom[key] = fiber.props[key]
     })
 
-  //处理子节点
-  element.props.children.forEach(child => {
-    render(child,dom);
-  })
-
-  container.appendChild(dom);
+  return dom;
 }
