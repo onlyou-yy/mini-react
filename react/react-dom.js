c80361c80a8ef4e7fb8194a26a49004b1c7a428c
练建发 <958823465@qq.com>
2023-04-27 10:29:14 +0800

    不在render阶段直接添加dom到页面上，而是增加一个commit阶段，当fiber全部处理完成之后再将fiber的dom添加到页面上

--- react/react-dom.js
+++ react/react-dom.js
@@ -1,18 +1,20 @@
 // 下一功能单元，是一个指针指向当前需要运行的功能单元
 let nextUnitOfWork = null;
-
+// fiber 根
+let wipRoot = null;
 /**
  * 渲染虚拟DOM到目标上
  * @param {VNode} element 虚拟DOM
  * @param {HTMLElement} container  目标节点
  */
 export function render(element,container){
-  nextUnitOfWork = {
+  wipRoot = {
     dom:container,
     props:{
       children:[element],
     }
   }
+  nextUnitOfWork = wipRoot;
 }
 
 /**开始工作
@@ -26,10 +28,37 @@ function workLoop(deadline){
     // 可以用时间小于 1 就让出控制权
     shouldYield = deadline.timeRemaining() < 1;
   }
+  //整个Fiber链都处理完成之后再把整个Fiber 渲染为DOM
+  if(!nextUnitOfWork && wipRoot){
+    commitRoot();
+  }
+
   // 空闲时间执行任务
   window.requestIdleCallback(workLoop);
 }
 window.requestIdleCallback(workLoop);
+
+/**
+ * 处理提交的fiber树
+ * @param {*} fiber 
+ */
+function commitWork(fiber){
+  if(!fiber){
+    return
+  }
+  // 将自己添加到父节点中
+  const domParent = fiber.parent.dom
+  domParent.appendChild(fiber.dom);
+  // 继续处理子节点和兄弟节点
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+// 提交任务，将fiber tree 渲染为真实 DOM
+function commitRoot(){
+  commitWork(wipRoot.child);
+  wipRoot = null;
+}
 
 /**执行单元任务
  * 1.将元素添加到 DOM
@@ -42,9 +71,11 @@ function performUnitOfWork(fiber){
     fiber.dom = createDom(fiber);
   }
   // 如果fiber有父节点，将fiber.dom添加到父节点
-  if(fiber.parent){
-    fiber.parent.dom.appendChild(fiber.dom);
-  }
+  // 因为任务可中断，而且每次只处理一个DOM，所以直接添加到页面上可能会出现，页面不完整的情况
+  // 可以等整个Fiber链都处理完成之后再把整个Fiber 渲染为DOM
+  // if(fiber.parent){
+  //   fiber.parent.dom.appendChild(fiber.dom);
+  // }
 
   // 获取到当前fiber的孩子节点
   const elements = fiber.props.children
